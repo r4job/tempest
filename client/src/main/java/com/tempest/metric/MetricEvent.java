c85fe0f472a383dee635bc31b12f1a283dceb59b
SaeronMeng <xmeng7@buffalo.edu>
2025-04-30 00:50:28 +0800

    Remove pojo package for MetricEvent and MetricServiceGrpc falling into same namespace

---
+++ client/src/main/java/com/tempest/metric/MetricEvent.java
@@ -0,0 +1,39 @@
+package com.tempest.metric;
+
+public abstract class MetricEvent {
+    private static final int COUNT_ONE = 1;
+    private final String objectType;
+    private final String itemId;
+    private final long timestamp;
+    private final int count;
+
+    public MetricEvent(String objectType, String itemId, long timestamp, int count) {
+        this.objectType = objectType;
+        this.itemId = itemId;
+        this.timestamp = timestamp;
+        this.count = count;
+    }
+
+    public MetricEvent(String objectType, String itemId, long timestamp) {
+        this.objectType = objectType;
+        this.itemId = itemId;
+        this.timestamp = timestamp;
+        this.count = COUNT_ONE;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public String getObjectType() {
+        return objectType;
+    }
+}
