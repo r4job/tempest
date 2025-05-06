a3e15397dea425f7bbece0ec74279e6d9d4e7134
SaeronMeng <xmeng7@buffalo.edu>
2025-05-06 23:23:38 +0800

    Reorder decorators to reflect emission workflow correctly

--- client/src/main/java/com/tempest/metric/MetricEmitterBuilder.java
+++ client/src/main/java/com/tempest/metric/MetricEmitterBuilder.java
@@ -30,44 +30,40 @@ public class MetricEmitterBuilder {
         return builder;
     }
 
-    public MetricEmitterBuilder withAsync(int asyncQueueCapacity) {
+    public void withAsync(int asyncQueueCapacity) {
         this.enableAsync = true;
         this.asyncQueueCapacity = asyncQueueCapacity;
-        return this;
     }
 
-    public MetricEmitterBuilder withBatch(int flushIntervalSec) {
+    public void withBatch(int flushIntervalSec) {
         this.enableBatch = true;
         this.flushIntervalSec = flushIntervalSec;
-        return this;
     }
 
-    public MetricEmitterBuilder withRetry(int maxRetries, long baseDelayMs) {
+    public void withRetry(int maxRetries, long baseDelayMs) {
         this.enableRetry = true;
         this.maxRetries = maxRetries;
         this.retryBaseDelayMs = baseDelayMs;
-        return this;
     }
 
-    public MetricEmitterBuilder withDurability(File file) {
+    public void withDurability(File file) {
         this.enableDurability = true;
         this.durabilityFile = file;
-        return this;
     }
 
     public MetricEmitter build() {
         MetricEmitter emitter = base;
-        if (enableAsync) {
-            emitter = new AsyncMetricEmitter(emitter, asyncQueueCapacity);
-        }
-        if (enableBatch) {
-            emitter = new ScheduledBatchMetricEmitter(emitter, flushIntervalSec);
-        }
         if (enableDurability) {
             emitter = new DurableMetricEmitter(emitter, durabilityFile);
         }
         if (enableRetry) {
             emitter = new RetryMetricEmitter(emitter, maxRetries, retryBaseDelayMs);
+        }
+        if (enableAsync) {
+            emitter = new AsyncMetricEmitter(emitter, asyncQueueCapacity);
+        }
+        if (enableBatch) {
+            emitter = new ScheduledBatchMetricEmitter(emitter, flushIntervalSec);
         }
         return emitter;
     }
