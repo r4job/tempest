61fb4404b0f0b2e93efbc5770564dbd170bbba4c
xmeng <xianglongmg@gmail.com>
2025-06-28 23:23:37 +0800

    Add fallback upon thread exceeding

--- common/src/main/java/com/tempest/common/ThreadPoolBuilder.java
+++ common/src/main/java/com/tempest/common/ThreadPoolBuilder.java
@@ -1,8 +1,25 @@
 package com.tempest.common;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class ThreadPoolBuilder {
+    private static final int CORES = Runtime.getRuntime().availableProcessors();
+    private static final int THREAD_LIMIT = CORES * 2;
+    private static final AtomicInteger totalThreads = new AtomicInteger(0);
+    private static final Logger logger = LoggerFactory.getLogger(ThreadPoolBuilder.class);
+
+    private static final ExecutorService GENERAL_POOL = ThreadPoolBuilder.newBuilder()
+            .named("shared")
+            .withCoreThreads(CORES)
+            .withMaxThreads(THREAD_LIMIT)
+            .withQueueCapacity(1000)
+            .withKeepAlive(60_000)
+            .build();
+
     private String name = "tempest-";
     private int corePoolSize = 1;
     private int maxPoolSize = corePoolSize;
@@ -10,6 +27,10 @@ public final class ThreadPoolBuilder {
     private int queueCapacity = 1000;
     private boolean isDaemon = true;
     private RejectedExecutionHandler rejectedHandler = new ThreadPoolExecutor.AbortPolicy();
+
+    public static ExecutorService general() {
+        return GENERAL_POOL;
+    }
 
     public static ThreadPoolBuilder newBuilder() {
         return new ThreadPoolBuilder();
@@ -51,6 +72,15 @@ public final class ThreadPoolBuilder {
     }
 
     public ExecutorService build() {
+        int requested = this.maxPoolSize;
+        int current = totalThreads.addAndGet(requested);
+
+        if (current > THREAD_LIMIT) {
+            logger.warn("[TempestThreadPool] Thread creation exceeds safe limit: {} > {}. "
+                    + "Reusing general pool.", current, THREAD_LIMIT);
+            return general();
+        }
+
         BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(queueCapacity);
         ThreadFactory factory = new NamedThreadFactory(name, isDaemon);
         return new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveMillis, TimeUnit.MILLISECONDS, queue, factory, rejectedHandler);
